8b6f43db779f26812919e759fb121444df33bb66
Rivaldy Fauzan <rifalous@gmail.com>
2019-06-26 15:53:11 +0700

    Add Class Hero, HeroAdapter, OnClickListener, etc.

---
+++ app/src/main/java/id/rifalous/mylistviewapps/Hero.java
@@ -0,0 +1,31 @@
+package id.rifalous.mylistviewapps;
+
+class Hero {
+    private int photo;
+    private String name;
+    private String description;
+
+    public int getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(int photo) {
+        this.photo = photo;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
--- app/src/main/java/id/rifalous/mylistviewapps/MainActivity.java
+++ app/src/main/java/id/rifalous/mylistviewapps/MainActivity.java
@@ -1,21 +1,65 @@
 package id.rifalous.mylistviewapps;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    private String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
+    //private String[] dataName = {"Cut Nyak Dien","Ki Hajar Dewantara","Moh Yamin","Patimura","R A Kartini","Sukarno"};
+    private String[] dataName;
+    private String[] dataDescription;
+    private TypedArray dataPhoto;
+    private HeroAdapter adapter;
+    private ArrayList<Hero> heroes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        adapter = new HeroAdapter(this);
+
         ListView listView = findViewById(R.id.lv_list);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        //                android.R.layout.simple_list_item_1, android.R.id.text1, dataName);
         listView.setAdapter(adapter);
+
+        prepare();
+        addItem();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, heroes.get(i).getName(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    private void prepare() {
+        dataName = getResources().getStringArray(R.array.data_name);
+        dataDescription = getResources().getStringArray(R.array.data_description);
+        dataPhoto = getResources().obtainTypedArray(R.array.data_photo);
+    }
+
+    private void addItem() {
+        heroes = new ArrayList<>();
+
+        for (int i = 0; i < dataName.length; i++) {
+            Hero hero = new Hero();
+            hero.setPhoto(dataPhoto.getResourceId(i, -1));
+            hero.setName(dataName[i]);
+            hero.setDescription(dataDescription[i]);
+            heroes.add(hero);
+        }
+
+        adapter.setHeroes(heroes);
     }
 }
